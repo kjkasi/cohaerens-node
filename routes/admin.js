d13fcf1b78b0d85127400139b521297783590f88
root <root@localhost.localdomain>
2016-08-04 11:59:16 +0300

    Пешел на mongodb, макет основного экрана плюс начало работы над админкой, авторизация

---
+++ routes/admin.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var router = express.Router();
+var passport = require('passport');
+var Account = require('./../models/account');
+
+router.get('/', function(req, res) {
+  res.render('admin-login', { user : req.user });
+});
+
+router.post('/', passport.authenticate('local', { failureRedirect: '/admin/error'}), function(req, res) {
+  res.redirect('/admin/index');
+});
+
+router.get('/error', function(req, res) {
+  res.render('admin-error', {title : "equilibrium"});
+});
+
+//need authorization verify
+router.get('/index', function(req, res) {
+  //if (!req.user) {
+    //res.redirect('/admin/error');
+  //};
+  res.render('admin-index', { user : req.user });
+});
+
+module.exports = router;
