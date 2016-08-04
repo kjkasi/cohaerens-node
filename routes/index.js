4eef1c6bb85de483339d088a54cc813b7fa24fb3
root <root@localhost.localdomain>
2016-08-04 21:21:42 +0300

    Работа над основной страницей

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
+var Cs = require('./../models/cs');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Сohaerens' });
+  
+  Cs.find({}, function(err, allCs) {
+    if (err) throw err;
+    //console.log(allCs);
+    res.render('index', { title: 'Сohaerens', "allCs": allCs});
+  });
+
+  //res.render('index', { title: 'Сohaerens' });
 });
+
+router.post('/', function(req, res) {
+  
+  //console.log(req.param);
+  res.json(req.body);
+
+});
+
 
 module.exports = router;
